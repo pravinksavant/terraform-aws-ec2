621d0ee79f6142daf8677d8ffefcffac6cacee11
Pravin Savant <pravin.savant@cyara.com>
2021-12-15 15:36:33 +0530

    modified

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ terraform {
 }
 
 resource "aws_instance" "ec2" {
-  ami               = var.ami_id
-  instance_type     = var.instance_type
+  ami               = "${var.ami_id}"
+  instance_type     = "${var.instance_type}"
        
 }
