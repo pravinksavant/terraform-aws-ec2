8c4817c412fc234d6aef620501fa9019b7b8d5fd
Pravin Savant <pravin.savant@cyara.com>
2021-12-15 10:22:16 +0530

    initial commit

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  type = string
+}
+
+variable "az" {
+  type = string
+}
+
+variable "ami_id" {
+  type = string
+}
+
+variable "keyname" {
+  type =  string
+}
+
+variable "instance_type" {
+  type = string
+}
